566c7ef906f63f67a24f2086f72d081eaf28650d
Paulo <pmarquesbaronefilho18@protonmail.com>
2023-05-14 19:01:05 -0300

    add calculate function and fix small bug layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
-import InputContainer from "./components/InputContainer/InputContainer";
-import ResponseData from "./components/ResponseData/ResponseData";
 import iconArrow from "./assets/img/icon-arrow.svg";
+import { useState } from "react";
 
 export default function App() {
-  const dataTypes = ['year', 'month', 'day', 'hour', 'minute', 'second'];
+  const [ data, setData ] = useState({ days: '--', months: '--', years: '--'});
+
+  const calculateDate = (e) => {
+    e.preventDefault()
+		const { day, month, year } = e.target.elements
+		const valueDate = {
+			day: Number(day.value),
+			month: Number(month.value - 1),
+			year: Number(year.value)
+		}
+
+    const currentDate = new Date(0);
+
+    const pastDate = new Date(valueDate.year, valueDate.month, valueDate.day);
+    const diffDate = new Date(Date.now() - pastDate.getTime());
+
+    setData({
+      years: Math.abs(diffDate.getUTCFullYear() - currentDate.getUTCFullYear()),
+      months: Math.abs(diffDate.getUTCMonth() - currentDate.getUTCMonth()),
+      days: Math.abs(diffDate.getUTCDate() + 1 - currentDate.getUTCDate())
+    });
+  }
 
   return (
-    <section className="bg-white px-6 py-12 rounded-3xl mt-20 mx-6">
-      <form className="flex flex-col gap-4 justify-between">
+    <section className="bg-white px-6 py-12 rounded-3xl rounded-br-[150px] mx-[5%] mt-20">
+      <form onSubmit={calculateDate} className="flex flex-col gap-4 justify-between">
         <div className="flex flex-row justify-between">
-          <InputContainer id="day" placeholder="dd" />
-          <InputContainer id="month" placeholder="mm" />
-          <InputContainer id="year" placeholder="dd" />
-        </div>
-        <div className="flex flex-row justify-between">
-          <InputContainer id="hour" placeholder="mm" />
-          <InputContainer id="minute" placeholder="mm" />
-          <InputContainer id="second" placeholder="ss" />
+          <div className="flex flex-col w-[30%]">
+            <label className="text-gray-500 tracking-[.25em] mb-1 text-sm" htmlFor="day">DIA</label>
+            <input required name="day" className="p-3 border-solid border-2 border-gray-300 rounded-md outline-none focus:border-purple" type="number" placeholder="DD" id="day" />
+          </div>
+          <div className="flex flex-col w-[30%]">
+            <label className="text-gray-500 tracking-[.25em] mb-1 text-sm" htmlFor="month">MÊS</label>
+            <input required name="month" className="p-3 border-solid border-2 border-gray-300 rounded-md outline-none focus:border-purple" type="number" placeholder="MM" id="month" />
+          </div>
+          <div className="flex flex-col w-[30%]">
+            <label className="text-gray-500 tracking-[.25em] mb-1 text-sm" htmlFor="year">ANO</label>
+            <input required name="year" className="p-3 border-solid border-2 border-gray-300 rounded-md outline-none focus:border-purple" type="number" placeholder="AAAA" id="year" />
+          </div>
         </div>
         <div className="relative flex justify-center items-center w-full h-[1px] bg-gray-300 my-10">
           <button type="submit" className="absolute bg-purple rounded-full p-4"><img className="w-8" src={iconArrow} alt="Ícone de uma seta apontada para baixo"/></button>
         </div>
       </form>
       <div className="mt-4">
-        {dataTypes.map((item) => {
-          return <ResponseData data="--" dataType={item} />
-        })}
+        <p className="font-extrabold italic text-4xl"><span className="text-purple">{data.years}</span> anos</p>
+        <p className="font-extrabold italic text-4xl"><span className="text-purple">{data.months}</span> meses</p>
+        <p className="font-extrabold italic text-4xl"><span className="text-purple">{data.days}</span> dias</p>
       </div>
     </section>
   );
--- src/components/ResponseData/ResponseData.jsx
+++
@@ -1,5 +0,0 @@
-export default function ResponseData(props) {
-  return (
-    <p className="font-extrabold italic text-4xl"><span className="text-purple">{props.data}</span> {props.dataType}</p>
-  )
-}
